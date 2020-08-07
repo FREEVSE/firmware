42d8016a32e9054226ec2817b90305b8ebf2e241
Matthew Goulart <matthew.goulart@gmail.com>
2020-08-06 21:32:13 -0400

    Cleaned up commented code

--- src/ControlPilot.cpp
+++ src/ControlPilot.cpp
@@ -13,16 +13,6 @@
     timerAlarmWrite(intTimer, t, true);\
     timerAlarmEnable(intTimer);
 
-/*#define SCHED_ACTION(t, a)\
-    timer_set_alarm_value(timer_group_t::TIMER_GROUP_0, timer_idx_t::TIMER_0, t);\
-    timer_isr_register(timer_group_t::TIMER_GROUP_0, timer_idx_t::TIMER_0, a, NULL, ESP_INTR_FLAG_IRAM, NULL);\
-    timer_enable_intr(timer_group_t::TIMER_GROUP_0, timer_idx_t::TIMER_0);    
-
-#define SCHED_ACTION_IN_ISR(t, a)\
-    REG_WRITE(TIMG_T0ALARMLO_REG, (int) a);\
-    REG_WRITE(TIMG_T0ALARMHI_REG, (int) (a >> 32));
-    */
-
 //Private
 DRAM_ATTR hw_timer_t * ControlPilot::intTimer;
 
@@ -41,19 +31,6 @@ void ControlPilot::Init(){
     ControlPilot::highTime = (int)(Configuration::GetCpPwmDutyCycle() * CP_PWM_FREQ);
     
     intTimer = timerBegin(CP_TIMER, 80, true);
-
-    /*
-    //Set up our timer. We use IDF here because arduino makes too many assumptions
-    timer_config_t tConfig;
-    tConfig.divider = 80; //Prescaler of 80 gives us a resolution of 1uS
-    tConfig.counter_dir = timer_count_dir_t::TIMER_COUNT_UP;
-    tConfig.counter_en = timer_start_t::TIMER_PAUSE;
-    tConfig.alarm_en = true;
-    tConfig.auto_reload = true;
-
-    timer_init(timer_group_t::TIMER_GROUP_0, timer_idx_t::TIMER_0, &tConfig);
-    timer_set_counter_value(timer_group_t::TIMER_GROUP_0, timer_idx_t::TIMER_0, 0x00000000ULL); //Timer will reset to this value on alarm
-    */
 }
 
 void IRAM_ATTR ControlPilot::PulseLow(){
